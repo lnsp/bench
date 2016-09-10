75b334bcea73bfe7c6254be56d2b39b0082a2692
Lennart Espe <lennart@mooxmirror.io>
2016-09-10 18:24:53 +0200

    chore: push version

--- main.go
+++ main.go
@@ -22,9 +22,9 @@ var (
 		Name: "bench",
 		Version: pkginfo.PackageVersion{
 			Major:      0,
-			Minor:      1,
+			Minor:      2,
 			Patch:      0,
-			Identifier: "release",
+			Identifier: "dev",
 		},
 	}
 )
