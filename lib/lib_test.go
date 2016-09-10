1470e729c19e39b5b6405ca6a57a67444ddb21a1
Lennart Espe <lennart@mooxmirror.io>
2016-09-10 18:26:56 +0200

    chore: add license notices

--- lib/lib_test.go
+++ lib/lib_test.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Lennart Espe. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE.md file.
+
 package lib
 
 import "testing"
